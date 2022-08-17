66b7cc22681a89a033eb07e26709497817d9e527
Lisa Heffernan <lisaheffernan@outlook.com>
2022-08-17 13:26:28 +0100

    can search for all by name or id

--- controller/reader.js
+++ controller/reader.js
@@ -10,16 +10,27 @@ exports.createReader = async (req, res) => {
   }
 };
 
+//not 100% happy with this, want to refactor it.
 exports.findAllReaders = async (req, res) => {
-  const queryString = req.query;
+  const queryString = req.query.name;
+
   try {
     if (!queryString) {
       const findAllReadersInDb = await Reader.findAll();
+      console.log('all ' + queryString);
       res.status(200).json(findAllReadersInDb);
     } else {
       console.log(queryString);
-      const findReader = await Reader.findAll({ where: queryString });
-      res.status(200).json(findReader);
+      const [findReaderByCondition] = await Reader.findAll({
+        where: {name: queryString}
+      });
+      if (!findReaderByCondition) {
+        console.log('error');
+        res.status(404).json({ error: "The reader could not be found." });
+      } else {
+        console.log('reader by name ' + queryString)
+        res.status(200).json(findReaderByCondition);
+      }
     }
   } catch (err) {
     res.sendStatus(500);
@@ -28,6 +39,14 @@ exports.findAllReaders = async (req, res) => {
 
 exports.findReaderById = async (req, res) => {
   const { readerId } = req.params;
-  const findReaderInDb = await Reader.findByPk(readerId);
-  res.status(200).json(findReaderInDb);
+  try {
+    const findReaderInDb = await Reader.findByPk(readerId);
+    if (!findReaderInDb) {
+      res.status(404).json({ error: "The reader could not be found." });
+    } else {
+      res.status(200).json(findReaderInDb);
+    }
+  } catch (err) {
+    res.sendStatus(500);
+  }
 };
